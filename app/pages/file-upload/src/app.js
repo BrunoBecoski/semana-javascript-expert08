77cef22bd2841723b35ca31d12bb3e5be6b0122d
Bruno Becoski <brunobecoski@gmail.com>
2023-10-29 01:01:25 -0300

    Criando os arquivos da pasta worker

--- app/pages/file-upload/src/app.js
+++ app/pages/file-upload/src/app.js
@@ -4,18 +4,36 @@ import View from './view.js';
 const view = new View()
 const clock = new Clock()
 
+const worker = new Worker('./src/worker/worker.js', {
+  type: 'module'
+})
+
+worker.onerror = (error) => {
+  console.error('error worker', error)
+}
+
+worker.onmessage = ({ data }) => {
+  if (data.status !== 'done') return;
+
+  clock.stop()
+  view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
+}
+
 let took = ''
 
 view.configureOnFileChange(file => {
+  const canvas = view.getCanvas()
+  worker.postMessage({
+    file,
+    canvas
+  }, [
+    canvas
+  ])
+ 
   clock.start((time) => {
     took = time;
     view.updateElapsedTime(`Process started ${time}`)
   })
-
-  setTimeout(() => {
-    clock.stop()
-    view.updateElapsedTime(`Process took ${took.replace('age', '')}`)
-  }, 5000 )
 })
 
 async function fakeFetch() {
